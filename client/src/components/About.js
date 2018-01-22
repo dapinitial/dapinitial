1974d4044aa8e89fc70a624501124980e0abd732
David Puerto <me@davidpuerto.com>
2018-01-22 11:30:06 -0800

    first commit

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -15,18 +15,16 @@ class About extends Component {
           </div>
           <p>BORN, Hasn't died, yet. "From Ketucky well-water to whale watching off my
             backporch," I'm not originally from Kentucky but I'm down with the unbridled
-            spirit, like I'm an apostle, reppin the Holy Spirit in Chicago. I like scars,
-            marks, and prison tattoos. Runaways, the homeless, victims of the sex trade, the
-            people that the web world exploits but the web industry won't retain. After
-            doing some faggy design shit for awhile, totally unamused by the pop culture
-            cock sucker designers and pussy ass fuck boys that wear their hats I decided to
-            learn JavaScript and I built this entire project in React. Check it out on
-            Github. After living in 8 states and two countries before I was eighteen years,
-            young me kept up the gypsy trend and tried out a myriad of lifestyles, before
-            arriving at clarity. Still shaping my future self along this road to perdition,
-            you can catch me outside, doin' it, waxing poetic, worldwide, across the United
-            States to Western Cape, the Patagonia, Andean Colombia, down unda, Tokyo to
-            Hakuba. How bout that?
+            spirit. I like scars, marks, and prison tattoos. Runaways, gypsies, military
+            brats, the homeless, victims of violence. If you don't realm here, that's okay,
+            we likely won't be lasting friends unless you're hot and flirty. You're welcome
+            to try me out though. I decided to learn JavaScript and I built this entire
+            project in React. Check it out on Github. After living in 8 states and two
+            countries before I was eighteen years, young me kept up the vegabond trend and
+            explored a myriad of lifestyles, before arriving at clarity. Still shaping my
+            future self along this road to perdition, you can catch me outside, doin' it,
+            waxing poetic, worldwide, across the United States to Western Cape, the
+            Patagonia, Andean Colombia, down unda, Tokyo to Hakuba. How bout that?
           </p>
 
         </div>
